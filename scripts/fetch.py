c4c7484b20945f9831879fc47f27f3820d5d6eef
AntonJohanssonn <158589266+AntonJohanssonn@users.noreply.github.com>
2025-09-02 13:02:15 +0200

    Update fetch.py

--- scripts/fetch.py
+++ scripts/fetch.py
@@ -8,7 +8,7 @@ import json, time
 from pathlib import Path
 import yfinance as yf
 
-TICKERS = ["AAPL", "MSFT", "ACB"]  # add/remove as you wish
+TICKERS = ["AAPL", "MSFT", "ACB", "HRTX" ]  # add/remove as you wish
 OUT_DIR = Path("data")
 OUT_DIR.mkdir(exist_ok=True)
 
